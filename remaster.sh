60172d47a66bb4d70c31ea4e688cdc788f9582b8
Diep Pham <me@favadi.com>
2018-04-21 15:13:54 +0700

    fixes initramfs

--- remaster.sh
+++ remaster.sh
@@ -27,29 +27,32 @@ unzip -q "$cz_path" -d "$tmp_dir"
 pushd "$tmp_dir/live"
 unsquashfs filesystem.squashfs
 mount --bind /dev/ "$tmp_dir/live/squashfs-root/dev"
-chroot "$tmp_dir/live/squashfs-root" /bin/bash <<EOF
+chroot "$tmp_dir/live/squashfs-root" /bin/bash <<'EOF'
 LC_ALL=C
 HOME=/root
+# assume that there is only one kernel installed
+kernel_path="$(find /lib/modules -mindepth 1 -maxdepth 1 -type d | head -1)"
+kernel_version="$(basename "$kernel_path")"
 
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t devpts none /dev/pts
 
+mkdir /boot
+
 mv /etc/resolv.conf /etc/resolv.conf.bak
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
 apt-get -qqy update
-apt-get -qqy dist-upgrade
-apt-get -qqy autoremove
-apt-get -qqy install git dkms
-apt-get -qqy install linux-headers-$(uname -r)
+apt-get -qqy install git dkms linux-headers-"$kernel_version"
+update-initramfs -k all -c
 
 echo -e "\n# macbook12-spi-drivers\napplespi\nappletb\nspi_pxa2xx_platform\nintel_lpss_pci" >> /etc/initramfs-tools/modules
 git clone https://github.com/roadrunner2/macbook12-spi-driver.git
 cd ./macbook12-spi-driver
 git checkout touchbar-driver-hid-driver
 dkms add .
-dkms install -m applespi -v 0.1
+dkms install -m applespi -v 0.1 -k "$kernel_version"
 
 mv /etc/resolv.conf.bak /etc/resolv.conf
 umount /dev/pts
@@ -57,6 +60,8 @@ umount /sys
 umount /proc
 EOF
 sudo umount "$tmp_dir/live/squashfs-root/dev"
+sudo find ./squashfs-root/boot -type f ! -name '*.old-dkms' | xargs -n 1 -I {} cp {} initrd.img
+rm -rf ./squashfs-root/boot
 
 blocksize=$(unsquashfs -s filesystem.squashfs | grep "Block size" | awk '{print $3}')
 rm -f filesystem.squashfs
